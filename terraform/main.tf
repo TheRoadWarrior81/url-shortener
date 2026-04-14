34527a55ce2303684fdecc31ec5a4162198b9874
Pranav Pasumarthy <53782063+TheRoadWarrior81@users.noreply.github.com>
2026-04-14 18:16:23 -0400

    Handle duplicate URLs via GSI lookup

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,7 +68,8 @@ resource "aws_iam_role_policy" "lambda_policy" {
         Action = [
           "dynamodb:PutItem",
           "dynamodb:GetItem",
-          "dynamodb:UpdateItem"
+          "dynamodb:UpdateItem",
+          "dynamodb:Query"
         ]
         Resource = aws_dynamodb_table.urls.arn
       },
@@ -155,6 +156,11 @@ resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
+
+  default_route_settings {
+    throttling_burst_limit = 50
+    throttling_rate_limit  = 100
+  }
 }
 
 resource "aws_apigatewayv2_integration" "shorten" {
